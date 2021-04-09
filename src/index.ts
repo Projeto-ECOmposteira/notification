0971b16a84adaf5a3aad8d668d8b7f200807a9f5
Welison Almeida <welison.almeida.923@gmail.com>
2021-04-09 12:51:50 -0300

    Config environment variables to project

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import loadEnvs from "./utils/secrets";
+
+loadEnvs();
 
 const app = express();
-const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server 2 Teste"));
+const PORT = process.env["PORT"] || 8000;
+app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
