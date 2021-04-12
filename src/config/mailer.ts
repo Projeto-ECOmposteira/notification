6a976144a405adde4969874ef3941cfbbfbfc4e9
Welison Almeida <welison.almeida.923@gmail.com>
2021-04-11 22:34:46 -0300

    Config nodemailer with handlebars to send e-mails

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -1,14 +1,33 @@
 import nodemailer from "nodemailer";
+import exphbs from "express-handlebars";
+import nodelmailerhbs from "nodemailer-express-handlebars";
 import logger from "../utils/logger";
+import { resolve } from "path";
 
 const transporter = nodemailer.createTransport({
-    host: process.env["SMTP_HOST"],
-    port: Number(process.env["SMTP_PORT"]),
-    auth: {
-        user: process.env["SMTP_EMAIL"],
-        pass: process.env["SMTP_PASSWORD"]
-    }
+  host: process.env["SMTP_HOST"],
+  port: Number(process.env["SMTP_PORT"]),
+  auth: {
+    user: process.env["SMTP_EMAIL"],
+    pass: process.env["SMTP_PASSWORD"],
+  },
 });
+
+const viewPath = resolve(__dirname, "..", "views");
+
+transporter.use(
+  "compile",
+  nodelmailerhbs({
+    viewEngine: exphbs.create({
+      layoutsDir: resolve(viewPath),
+      partialsDir: resolve(viewPath, "partials"),
+      defaultLayout: "index",
+      extname: ".hbs",
+    }),
+    viewPath,
+    extName: ".hbs",
+  })
+);
 
 const SMTPConnectionValidatation = (error: Error) => {
   if (error) {
